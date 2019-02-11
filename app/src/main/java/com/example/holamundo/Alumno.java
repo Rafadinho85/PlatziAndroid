68514e49e6f0a313a94db72ef06e3f35c373f657
Jery Ramírez <rafa85jogo@gmail.com>
2019-02-11 10:26:26 -0600

    Initial commit

---
+++ app/src/main/java/com/example/holamundo/Alumno.java
@@ -0,0 +1,34 @@
+package com.example.holamundo;
+
+import java.util.Date;
+
+public class Alumno {
+
+    private String nombre;
+    private long numero_cuenta;
+    private Date fechaNacimiento;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public long getNumero_cuenta() {
+        return numero_cuenta;
+    }
+
+    public void setNumero_cuenta(long numero_cuenta) {
+        this.numero_cuenta = numero_cuenta;
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+}
